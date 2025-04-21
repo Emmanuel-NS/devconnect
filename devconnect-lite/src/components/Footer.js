707896aed163a8c8666baddb98380a04998b35a7
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-04-21 12:27:05 +0200

    Delete devconnect-lite directory

--- devconnect-lite/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="container">
-        <p>&copy; {new Date().getFullYear()} DevConnect Lite</p>
-      </footer>
-    );
-  }
-  
--- devconnect-lite/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div className="container">
-        <h1>Welcome to DevConnect Lite</h1>
-        <p>Your showcase for portfolios & blog posts.</p>
-      </div>
-    );
-  }
-  
